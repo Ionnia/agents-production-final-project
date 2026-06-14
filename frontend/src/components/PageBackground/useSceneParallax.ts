77f6cce58a06220c5ff18a93684eed19710a12e6
Ionnia <baymakanov@gmail.com>
2026-06-14 19:39:36 +0300

    chore(frontend): remove old torn-magazine background system and assets

--- frontend/src/components/PageBackground/useSceneParallax.ts
+++
@@ -1,9 +0,0 @@
-import { useParallax } from './useParallax'
-
-export function useSceneParallax() {
-  const { parallaxStyle: skyLayerStyle } = useParallax('sky')
-  const { parallaxStyle: midLayerStyle } = useParallax('mid')
-  const { parallaxStyle: foregroundLayerStyle } = useParallax('foreground')
-
-  return { skyLayerStyle, midLayerStyle, foregroundLayerStyle }
-}
